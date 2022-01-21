fbc5d66e6d61b74f3ebc8a50af9b9409501928ba
miffy <plus@outlook.at>
2022-01-21 17:19:04 +0800

    learn lesson 13

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -3,6 +3,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
 import ky from "kyouka";
+import { AxesHelper } from "three";
 
 /**
  * Base
@@ -16,11 +17,84 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// Axis Helper
+// const axisHelper = new THREE.AxesHelper()
+// scene.add(axisHelper)
+
+
 /**
  * Textures
  */
-const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("/textures/matcaps/8.png");
+const textureLoader = new THREE.TextureLoader()
+
+const backgroundMatcapTexture = textureLoader.load('./textures/matcaps/2.png')
+const backgroundMaterial = new THREE.MeshMatcapMaterial({ matcap: backgroundMatcapTexture})
+
+const matcapTexture = textureLoader.load('./textures/matcaps/8.png')
+const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture})
+
+const donutArray = []
+
+const handleFontLoad = (font) => {
+  // console.log(font);
+  const textGeometry = new THREE.TextBufferGeometry(
+    // window.prompt("请输入你想展示的文字\nInput the text you want to display",'Hello Three.js!'),
+    'Hello Three.js!',
+    {
+      font,
+      size: 0.5,
+      height: 0.2,
+      curveSegments: 5,
+      bevelEnabled: true,
+      bevelThickness: 0.03,
+      bevelSize: 0.02,
+      bevelOffset: 0,
+      bevelSegments: 3,
+    }
+  );
+  const text = new THREE.Mesh(textGeometry, textMaterial);
+  textGeometry.computeBoundingBox()
+  console.log(textGeometry.boundingBox);
+  textGeometry.translate(
+    - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+    - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+    - (textGeometry.boundingBox.max.z - 0.03) * 0.5,
+  )
+
+
+  scene.add(text)
+
+  console.time('donuts');
+
+  const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45)
+
+  
+  for(let i = 0; i < 300; i++){
+    const donut = new THREE.Mesh(donutGeometry, backgroundMaterial)
+
+    donut.position.x = (Math.random() - 0.5 ) * 10
+    donut.position.y = (Math.random() - 0.5 ) * 10
+    donut.position.z = (Math.random() - 0.5 ) * 10
+
+    donut.rotation.x = Math.random() * Math.PI
+    donut.rotation.y = Math.random() * Math.PI
+    
+    const scale = Math.random() * 0.2
+    donut.scale.set(scale, scale, scale)
+
+    scene.add(donut)
+
+    donutArray.push(donut)
+  }
+
+  console.timeEnd('donuts');
+}
+
+const fontLoader = new THREE.FontLoader()
+fontLoader.load(
+  './fonts/helvetiker_regular.typeface.json',
+  (font) => handleFontLoad(font)
+)
 
 /**
  * Object
@@ -92,50 +166,17 @@ const tick = () => {
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
+
+  for(let i = 0; i < 300; i += 1){
+    donutArray[i].position.x += (Math.random() - 0.5 ) * 0.1
+    donutArray[i].position.y += (Math.random() - 0.5 ) * 0.1
+    donutArray[i].position.z += (Math.random() - 0.5 ) * 0.1
+
+    // donutArray[i].rotation.x += Math.random() * Math.PI
+    // donutArray[i].rotation.y += Math.random() * Math.PI
+  }
+
+
 };
 
 tick();
-
-const fontLoader = new THREE.FontLoader();
-
-fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
-  const textGeo = new THREE.TextBufferGeometry("Hello three.js", {
-    font,
-    size: 0.5,
-    height: 0.2,
-    curveSegments: 12,
-    bevelEnabled: true,
-    bevelThickness: 0.03,
-    bevelSize: 0.02,
-    bevelOffset: 0,
-    bevelSegments: 5,
-  });
-  const mat = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
-  const text = new THREE.Mesh(textGeo, mat);
-  scene.add(text);
-  textGeo.computeBoundingBox();
-  //   console.log(textGeometry.boundingBox);
-  //   textGeometry.translate(
-  //     -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
-  //     -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
-  //     -(textGeometry.boundingBox.max.z - 0.03) * 0.5
-  //   );
-  textGeo.center();
-
-  const donutGeo = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
-  for (let i = 0; i < 100; i++) {
-    const donut = new THREE.Mesh(donutGeo, mat);
-    scene.add(donut);
-    donut.position.copy(
-      new THREE.Vector3(
-        ky.randomNumberInRange(-0.5, 0.5) * 10,
-        ky.randomNumberInRange(-0.5, 0.5) * 10,
-        ky.randomNumberInRange(-0.5, 0.5) * 10
-      )
-    );
-    donut.rotation.x = ky.randomNumberInRange(0, 1) * ky.deg2rad(180);
-    donut.rotation.y = ky.randomNumberInRange(0, 1) * ky.deg2rad(180);
-    const scale = ky.randomNumberInRange(0, 1);
-    donut.scale.set(scale, scale, scale);
-  }
-});
